4279cb73937d7a8306ce6c66e020df25dd76c47f
` <dthuya17007@cusc.ctu.edu.vn>
2019-10-01 09:52:17 +0700

    save

--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatTableDataSource, MatSort } from '@angular/material';
+import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
 import { Exercise } from '../../auth/model/Exercise.model';
 import { TrainingService } from '../../auth/service/training.service';
 @Component({
@@ -10,12 +10,23 @@ import { TrainingService } from '../../auth/service/training.service';
 export class PastTrainingsComponent implements OnInit {
 
   displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
+
   dataSource = new MatTableDataSource<Exercise>();
-  @ViewChild(MatSort) sort: MatSort;
-  constructor(private trainingService: TrainingService) {}
+
+  @ViewChild(MatSort, { static: false }) sort: MatSort;
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+  constructor(private trainingService: TrainingService) { }
 
   ngOnInit() {
     this.dataSource.data = this.trainingService.getCompletedOrCancelledExercises();
   }
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
 
+  doFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
